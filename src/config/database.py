b6426557659950e1c9431749852da4e48331e454
Lucas Loeffler <lucasgloeffler@gmail.com>
2023-11-26 23:53:08 -0300

    Atualização em Routes e Config

--- src/config/database.py
+++ src/config/database.py
@@ -4,8 +4,8 @@ from sqlmodel import SQLModel, Session, create_engine
 PG_USERNAME = "postgres"
 PG_PASSWORD = "postgres"
 PG_HOST = "localhost"
-PG_PORT = 5433
-PG_DATABASE = "postgres"
+PG_PORT = 5432
+PG_DATABASE = "ProjetoWeb"
 
 connect_args = {}
 db_url = f"postgresql://{PG_USERNAME}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}"
